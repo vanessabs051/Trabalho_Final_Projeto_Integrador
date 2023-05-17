2d7382398a2728386ede1d2c670ae3e18297bd04
aluno <aluno@ubunturails.myguest.virtualbox.org>
2023-05-17 10:54:53 -0300

    layout das tabelas

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -65,6 +65,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:nome, :telefone, :email, :senha)
+      params.require(:user).permit(:nome, :telefone, :email, :password)
     end
 end
