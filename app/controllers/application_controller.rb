defe2cd6caf5f7bb798e966b05bdc65d04e43353
aluno <aluno@ubunturails.myguest.virtualbox.org>
2023-05-30 07:56:15 -0300

    layout de login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
-    
+    layout :layout
+
+    def layout
+        if current_user != nil
+            @layout = "application"
+        else
+            @layout = "applicationlogin"
+      
+        end
+    end
 end
