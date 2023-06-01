328c2dfa332a0e66ef7e9f609297a414dfe320ac
aluno <aluno@ubunturails.myguest.virtualbox.org>
2023-06-01 09:20:27 -0300

    Trabalho concluido com sucesso:)

--- config/routes.rb
+++ config/routes.rb
@@ -18,4 +18,6 @@ Rails.application.routes.draw do
   end
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+root to: 'users#index'
+
 end
